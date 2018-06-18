a92bcb05432e4e64ef39e9979e82643e87231e61
sumit goyal <goyalsumit093@gmail.com>
2018-06-18 08:16:54 +0530

    irst

---
+++ vendor/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class VendorDetail(models.Model):
+    name = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    description = models.TextField(max_length=200, null = True, blank = True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
