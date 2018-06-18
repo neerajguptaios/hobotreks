9fec42137d5aa9dad8f118a745f007fe405c3797
Neeraj Gupta <neeraj@cuddll.com>
2018-06-18 08:33:10 +0530

    virtual env added

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/niku/Projects/PythonProjects/python3/hobotreks/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
